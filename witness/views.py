27a3747c934b7a355a25fdf32aca42f66c9171b7
Sarah MacAdam <sam8bz@virginia.edu>
2015-11-05 20:07:08 -0500

    semi functional login and register

--- witness/views.py
+++ witness/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.contrib import auth
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+
 
 from django.http import HttpResponse
 
@@ -12,23 +16,58 @@ def super(request):
 def home(request):
     return render_to_response("witness/home.html", {'hello': "Hello World"})
 
-#def login_view(request):
-#    username = request.POST.get('username', '')
-#    password = request.POST.get('password', '')
-#    user = auth.authenticate(username=username, password = password)
-#    if user is not None and user.is_active:
-#        #correct password, user is active
-#        auth.login(request, user)
-#        #direct to success page
-#        return HttpResponseRedirect("/account/loggedin")
-#    else:
-#        #error page
-#        return HttpResponseRedirect("/account/invalid")
-#        #return render_to_response("witness/login.html", {'hello':"Login here"})
+def login(request):
+    return render_to_response("witness/login.html", {'hello':"Login here"})
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password = password)
+    if user is not None and user.is_active:
+        #correct password, user is active
+        auth.login(request, user)
+        #direct to success page
+        #return HttpResponseRedirect("/account/loggedin")
+    else:
+        #error page
+        return HttpResponseRedirect("/account/invalid")
+        #return render_to_response("witness/login.html", {'hello':"Login here"})
 
-#def loqout_view(request):
-#    auth.logout(request)
-#    return HttpResponseRedirect("/account/loggedout/")
+def logout(request):
+    auth.logout(request)
+    return render_to_response("witness/logout.html")
+
+def register(request):
+    return render_to_response("witness/register.html", {'hello': "Register here"})
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    email = request.POST.get('email', '')
+    reporter = Reporter.user.objects.create_user(username=username,email=email, password = password)
+    reporter.save()
+    if user is not None and user.is_active:
+        #correct password, user is active
+        auth.login(request, user)
+        #direct to success page
+        #return HttpResponseRedirect("/account/loggedin")
+    else:
+        #error page
+        return HttpResponseRedirect("/account/invalid")
+        #return render_to_response("witness/login.html", {'hello':"Login here"})
+
+
+
+
+
 
 #def register(request):
-#    return render_to_response("witness/register.html", {'hello': "Register here"})
+#    if request.method == 'POST':
+#        form = UserCreationForm(request.POST)
+#        if form.is_valid():
+#            new_user = form.save()
+#            return HttpResponseRedirect("/books/")
+#    else:
+#        form = UserCreationForm()
+#    return render(request, "registration/register.html", {
+#        'form': form,
+#    })
+
+
+
