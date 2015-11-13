fff559cac547825678a51feb2bf6d48728c260bb
Sarah MacAdam <sam8bz@virginia.edu>
2015-11-13 15:27:06 -0500

    integrating user and reporter

--- securewitness/settings.py
+++ securewitness/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'securewitness.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'securedb',
+        'NAME': 'mydatabase',
 	'USER': 'postgres',
-	'PASSWORD': 'syracuse9',
+	'PASSWORD': 'popsicle4',
 	'HOST': 'localhost',
 	'PORT': '5432',
     }
@@ -110,9 +110,9 @@ STATIC_URL = '/static/'
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
-DATABASES['default']['NAME'] = 'securedb'
+DATABASES['default']['NAME'] = 'mydatabase'
 DATABASES['default']['USER'] = 'postgres'
-DATABASES['default']['PASSWORD'] = 'syracuse9'
+DATABASES['default']['PASSWORD'] = 'popsicle4'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
