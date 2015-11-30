5d58e6e23e4c08b08361548d795882596423e0fd
Ryan Bannon <rtb7rd@virginia.edu>
2015-11-30 16:05:22 -0500

    fda first draft

---
+++ securewitness/fda.py
@@ -0,0 +1,27 @@
+import psycopg2
+from django import db
+
+#username = input("Enter username: ")
+#password = input("Enter password: ")
+
+#print(db.connections.databases)
+
+conn = psycopg2.connect("dbname ='newdb' user='postgres' host='localhost' password='p3ngu1n'")
+cur = conn.cursor()
+cur.execute("""SELECT report_title from report_report""")
+titles = cur.fetchall()
+
+for title in titles:
+    print(title[0])
+
+wanted = input("Type the name of the report you would like to view:")
+print("Title: ", wanted)
+cur.execute("SELECT report_short_description from report_report where report_title = %s", [wanted])
+shortdes = cur.fetchall()
+print("Short Description: ", shortdes[0][0])
+
+cur.execute("SELECT report_long_description from report_report where report_title = %s", [wanted])
+longdes = cur.fetchall()
+print("Long Description: ", longdes[0][0])
+
+
